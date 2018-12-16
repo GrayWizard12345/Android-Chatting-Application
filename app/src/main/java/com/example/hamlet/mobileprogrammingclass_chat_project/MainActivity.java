b07c3357ea2c66b23e84a3c9c734cb46fcad5318
hamlet <muslim4ik14@gmail.com>
2018-12-17 00:22:35 +0500

    Some work on custom action bar

--- app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/MainActivity.java
+++ app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hamlet.mobileprogrammingclass_chat_project;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        View view =getSupportActionBar().getCustomView();
+
+        ImageButton imageButton= (ImageButton)view.findViewById(R.id.action_bar_back);
     }
 }
