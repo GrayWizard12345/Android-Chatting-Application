60ad4cfc2aa4335aa2edffa693d396176f75eccd
hamlet <muslim4ik14@gmail.com>
2018-12-18 14:56:44 +0500

    Some work on custom action bar, Creation of basic blue print classes, Work on chat action bar and chat_list_item view

--- app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/MainActivity.java
+++ app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.hamlet.mobileprogrammingclass_chat_project.activities;
 
-import android.support.v7.app.ActionBar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 import com.example.hamlet.mobileprogrammingclass_chat_project.R;
 
 public class MainActivity extends AppCompatActivity {
