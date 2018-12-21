4b44f6a84b8df5a7f866d786b6c99c289c9374a7
Nozima <nozima.sb@gmail.com>
2018-12-21 08:00:33 +0500

    Holy Changes! Login Activity xml & java files; Registration Activity xml & java files; colors are added;

--- app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/LoginActivity.java
+++ app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.hamlet.mobileprogrammingclass_chat_project.activities;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,23 +29,118 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.example.hamlet.mobileprogrammingclass_chat_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity {
 
+    private EditText editText_phoneNum;
+    private EditText editText_password;
+    private CheckBox checkBox_password;
+    private ImageButton imageButton_done;
+    private TextView textView_linkRegister;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        init();
+        showHidePassword(); //to show or hide symbols in edittext when password is typing
+
+
+        imageButton_done.setOnClickListener(myListener);
+        textView_linkRegister.setOnClickListener(regActivity);
+
+
     }
+
+
+    public void init() {
+        editText_phoneNum = findViewById(R.id.editText_phoneNumber);
+        editText_password = findViewById(R.id.editText_passw);
+        checkBox_password = findViewById(R.id.checkbox_passw);
+        imageButton_done = findViewById(R.id.imageButton_donebtn);
+        textView_linkRegister = findViewById(R.id.linkToRegister);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+
+    private void showHidePassword() {
+        checkBox_password.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    editText_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else
+                    editText_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+            }
+        });
+
+    }
+
+
+    View.OnClickListener regActivity = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener myListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String phoneNum = editText_phoneNum.getText().toString();
+            String password = editText_password.getText().toString();
+
+            if (TextUtils.isEmpty(phoneNum) || TextUtils.isEmpty(password)) {
+                Toast.makeText(LoginActivity.this, "Please Fill ALL fields!", Toast.LENGTH_SHORT).show();
+            } else {
+
+                auth.signInWithEmailAndPassword(phoneNum, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Auth Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+            }
+        }
+    };
 }
+
--- app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/hamlet/mobileprogrammingclass_chat_project/activities/RegistrationActivity.java
@@ -1,14 +1,134 @@
 package com.example.hamlet.mobileprogrammingclass_chat_project.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.example.hamlet.mobileprogrammingclass_chat_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText et_name;
+    private EditText et_email;
+    private EditText et_phoneNumber;
+    private EditText et_password;
+    private ImageButton done_btn;
+    private CheckBox checkBox_password;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        init();
+        showHidePassword();
+
+        done_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = et_name.getText().toString();
+                String phone = et_phoneNumber.getText().toString();
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+
+                if (TextUtils.isEmpty(phone) || TextUtils.isEmpty(password)|| TextUtils.isEmpty(email) || TextUtils.isEmpty(name) ) {
+                    Toast.makeText(RegistrationActivity.this, "Please Fill ALL fields!", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 6) {
+                    Toast.makeText(RegistrationActivity.this, "Password length should be at least 6", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    register(name, phone, email, password);
+                }
+            }
+        });
     }
+
+
+    public void init() {
+        et_name = findViewById(R.id.editText_name);
+        et_email = findViewById(R.id.editText_email);
+        et_phoneNumber = findViewById(R.id.editText_phoneNumber);
+        et_password = findViewById(R.id.editText_passw);
+        done_btn = findViewById(R.id.imageButton_donebtn);
+        checkBox_password = findViewById(R.id.checkbox_passw);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    private void showHidePassword() {
+        checkBox_password.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    et_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else
+                    et_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+            }
+        });
+
+    }
+
+    private void register(final String name, final String phoneNum, final String email, String password) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    assert firebaseUser != null;
+                    String userid = firebaseUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(name);
+
+                    HashMap<String, String> hashmap = new HashMap<>();
+                    hashmap.put("name", name);
+                    hashmap.put("phoneNum", phoneNum);
+                    hashmap.put("email", email);
+
+                    reference.setValue(hashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Not possible!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
 }
